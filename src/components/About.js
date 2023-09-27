ac4ea6501c90e8620a4b572306dfd744d9cc728a
4me7h <4573r10n@gmail.com>
2023-09-27 13:33:49 -0600

    text: about content

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,12 @@ const About = () => {
                             <div className="about__container">
                                 <div className="about__col">
                                     <h3>SOMOS BLACK DIAMOND</h3>
+                                    <p>
+                                        Estamos enfocados en la rifa y sorteo de vehículos de alta gama para que por medio de poca inversión puedas tener el coche que siempre has soñado.
+                                    </p>
+                                    <p>
+                                        Somos una empresa 100% mexicana ubicada en Guadalajara Jalisco dedicada al sorteo y rifa de vehículos de gama premium.
+                                    </p>
                                 </div>
                                 <div className="about__col">
 
